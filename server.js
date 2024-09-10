eaddd0f349b8583c0d94e04d973eb249a56040e3
Ayobami Oluwadeyi <ayobamifiyibomi@gmail.com>
2024-09-10 23:17:23 +0100

    completed lesson 4

--- server.js
+++ server.js
@@ -1,7 +1,46 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-	console.log('request made');
+	console.log(req.url, req.method);
+
+	// Set Content-Type
+	res.setHeader('Content-Type', 'text/html');
+
+	// Setting up routing, so browser can display the page a user visits
+	let path = './views/';
+	switch(req.url) {
+		case '/':
+			path += 'index.html';
+			res.statusCode = 200; // Status code
+			break;
+		case '/about':
+			path += 'about.html';
+			res.statusCode = 200;
+			break;
+		// Using redirections
+		case '/about-me':
+			res.statusCode = 301;
+			res.setHeader('Location', '/about');
+			res.end();
+			break;
+		default:
+			path += '404.html';
+			res.statusCode = 400;
+			break;
+	}
+
+	// Read html file and send the content of the file to the browser
+	fs.readFile(path, (err, data) => {
+		if (err) {
+			console.log(err);
+			res.end();
+		}
+		else {
+			// res.write(data);
+			res.end(data);
+		}
+	})
 });
 
 
