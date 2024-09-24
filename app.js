b35625b10adb52810a1411e319e56eccc7073f00
Ayobami Oluwadeyi <ayobamifiyibomi@gmail.com>
2024-09-24 20:28:59 +0100

    Using Mongodb

--- app.js
+++ app.js
@@ -1,52 +1,86 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 
 // Setting up express app
 const app = express();
 
+// Connect to mongodb
+const dbURI = 'mongodb://127.0.0.1:27017/node-tuts';
+mongoose.connect(dbURI)
+    .then((result) => app.listen(2000))
+    .catch((err) => console.log(err));
+
 // Register view engine
 app.set('view engine', 'ejs');
-
 
 // Listen for requests
 app.listen(3000);
 
 // Middleware & Static files
 app.use(express.static('public'));
-
-// Using morgan middleware
 app.use(morgan('dev'));
 
-// Making a middleware to log information on the console
-// app.use((req, res, next) => {
-//     console.log('New request made:');
-//     console.log('Host: ', req.hostname);
-//     console.log('Path: ', req.path);
-//     console.log('Method: ', req.method);
-//     // Using next() to stop the middleware and move on to the next handler
-//     next();
-// });
+// Mongoose and Mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'New Blog',
+        snippet: 'About my new blog',
+        body: 'More about my new blog'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+// Retrieving all the blogs
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+// Retrieving a single blog using the blog's id
+app.get('/single-blog', (req, res) => {
+    Blog.findById('66f202829e4364fced0ccb3d')
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 
-// Respond to requests
+// Routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur."},
-        {title: "Mari finds stars", snippet: "Lorem ipsum dolor sit amet consectetur."},
-        {title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur."}
-    ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
-
-// This middleware doesn't gets logged in the console as it is below the path we want to view
-// app.use((req, res, next) => {
-//     console.log('In the next middleware');
-//     next();
-// });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
+
+app.get('/blogs', (req,res) => {
+    Blog.find().sort({ createdAt: -1})
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 // Redirects & 404 page
 app.get('/blogs/create', (req, res) => {
